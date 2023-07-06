35d0cea7bce159973d5676115a581e8c4ed93866
AytacSahin <haytacsahin@gmail.com>
2023-07-06 14:43:39 +0300

    sanity test done

---
+++ api/posts/posts.test.js
@@ -0,0 +1,16 @@
+const postModel = require('./posts-model');
+const db = require('../../data/db-config');
+
+beforeAll(async () => {
+    await db.migrate.rollback();
+    await db.migrate.latest();
+});
+
+beforeEach(async () => {
+    await db.seed.run();
+})
+
+
+test('Sanity Test', ()=>{
+    expect(true).toEqual(true); 
+})
